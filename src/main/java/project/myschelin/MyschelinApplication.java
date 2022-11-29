fe786f1b596c025b6dac23cdeee810a2563c29b6
ybkim-dev <beanybigggy@gmail.com>
2022-11-29 18:15:59 +0900

    init project

---
+++ src/main/java/project/myschelin/MyschelinApplication.java
@@ -0,0 +1,13 @@
+package project.myschelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyschelinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyschelinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/myschelin/MyschelinApplicationTests.java
@@ -0,0 +1,13 @@
+package project.myschelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyschelinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
