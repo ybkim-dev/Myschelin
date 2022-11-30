1da7d1ae0c6d502f82bed7321640d7d1948f8420
ybkim-dev <beanybigggy@gmail.com>
2022-11-30 14:30:58 +0900

    fix : imagePath 변수명 변경

--- src/main/java/project/myschelin/domain/store/dto/StoreInsertDto.java
+++ src/main/java/project/myschelin/domain/store/dto/StoreInsertDto.java
@@ -2,5 +2,5 @@ package project.myschelin.domain.store.dto;
 
 import project.myschelin.domain.store.model.Category;
 
-public record StoreInsertDto(String name, String description, Category category, String image_path) {
+public record StoreInsertDto(String name, String description, Category category, String imagePath) {
 }
